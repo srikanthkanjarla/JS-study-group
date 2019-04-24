3eefdf89f4df7437a703db9491d9e835fe52f698
Eliot Sanford <eliot587@gmail.com>
2019-04-24 17:23:08 -0500

    clean js file

---
+++ 20190424fccJS-clean.js
@@ -0,0 +1,20 @@
+
+
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+    
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  // Change this value to test
+  console.log(testLessThan(10));
+
+  console.log(testLessThan(26));
+
+  console.log(testLessThan(56));
