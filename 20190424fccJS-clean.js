ca1e9b7cad7ebcd62c0594307a9ef1b63d13837f
Eliot Sanford <eliot587@gmail.com>
2019-04-24 23:51:08 -0500

    saved at overriding the default props

--- 20190424fccJS-clean.js
+++ 20190424fccJS-clean.js
@@ -1,5 +1,7 @@
 
 
+//Basic JavaScript: Comparison with the Less Than Operator
+
 function testLessThan(val) {
     if (val < 25) {  // Change this line
       return "Under 25";
@@ -13,8 +15,33 @@ function testLessThan(val) {
   }
   
   // Change this value to test
-  console.log(testLessThan(10));
+//   console.log(testLessThan(10));
 
-  console.log(testLessThan(26));
+//   console.log(testLessThan(26));
 
-  console.log(testLessThan(56));
+//   console.log(testLessThan(56));
+
+  
+// less than or equal to
+
+
+function testLessOrEqual(val) {
+    if (val <= 12) {  // Change this line
+      return "Smaller Than or Equal to 12";
+    }
+    
+    if (val <= 24) {  // Change this line
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+  }
+  
+  // Change this value to test
+  testLessOrEqual(10);
+
+  console.log(testLessOrEqual(10));
+
+  console.log(testLessOrEqual(25));
+
+  console.log(testLessOrEqual(23));
